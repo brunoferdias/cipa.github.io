e9a6d06a7980d02d011e35abf6087adcab30103b
brunoferdias <bruno_ferdiass12@outlook.com>
2024-07-26 14:58:27 -0300

    1

--- lib/server/server.dart
+++ lib/server/server.dart
@@ -1,3 +1,4 @@
 final keyApplicationId = 'faru8645N4L3z6vNNtfzVvpymjWIr6J29sc9YX7Y';
 final keyClientKey = 'p0VqpyNTIZ0JZvqYaVeZogeA6lqaNF6jtMn9SIuD';
 final keyParseServerUrl = 'https://parseapi.back4app.com';
+//1
